84c84d6896d613ee66e870f9a30fc9ca1bf37b0b
NVK <Dragontailonfire@users.noreply.github.com>
2025-12-08 23:08:32 +0530

    Fix styling issues

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -155,25 +155,37 @@ export function App() {
 
   if (!session.value) {
     return (
-      <div id="app" class="container text-center m-5 p-5">
-        <h1 class="m-5">Please log in to view notes</h1>
-        <button class="btn btn-dark" onClick={login}>
-          <img
-            src={githubMark}
-            alt="GitHub Invertocat logo"
-            class="github-mark"
-            width="20"
-            height="20"
-          />{" "}
-          Sign in with GitHub
-        </button>
+      <div id="app">
+        <div class="container my-5">
+          <div class="position-relative p-5 text-center text-muted bg-body border border-dashed rounded-5">
+            <h1 class="text-body-emphasis">Login to Simple Notes App</h1>
+            <p class="col-lg-6 mx-auto mb-4">
+              Please login using your GitHub account to manage your important
+              notes easily.
+            </p>
+            <button
+              class="btn btn-dark px-5 mb-5"
+              onClick={login}
+              type="button"
+            >
+              <img
+                src={githubMark}
+                alt="GitHub Invertocat logo"
+                class="github-mark"
+                width="25"
+                height="25"
+              />{" "}
+              Sign in with GitHub
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
 
   return (
-    <div id="app" class="container">
-      <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+    <div id="app">
+      <nav class="navbar navbar-expand-lg bg-body fixed-top">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">
             <svg
@@ -237,63 +249,73 @@ export function App() {
         </div>
       </nav>
       <div class="container text-center mt-5">
-        <form onSubmit={addNote} class="row mt-5 g-3">
-          <div class="input-group mb-3">
-            <input
-              id="add-note"
-              name="add-note"
-              value={newNote.value}
-              onInput={(e) => (newNote.value = e.currentTarget.value)}
-              placeholder="Add a new note..."
-              class="form-control form-control-lg"
-              type="text"
-              aria-label="Add new note"
-            />
-            <button
-              type="submit"
-              class="btn btn-lg btn-primary"
-              aria-label="Add note"
-              disabled={newNote.value === ""}
-              aria-disabled={newNote.value === "" ? "true" : "false"}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                class="bi bi-plus-circle"
-                viewBox="0 0 16 16"
-              >
-                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
-              </svg>
-            </button>
+        <div class="row mt-5">
+          <div class="col mt-5">
+            <form onSubmit={addNote}>
+              <div class="input-group mb-3">
+                <input
+                  id="add-note"
+                  name="add-note"
+                  value={newNote.value}
+                  onInput={(e) => (newNote.value = e.currentTarget.value)}
+                  placeholder="Add a new note..."
+                  class="form-control form-control-lg"
+                  type="text"
+                  aria-label="Add new note"
+                />
+                <button
+                  type="submit"
+                  class="btn btn-lg btn-primary"
+                  aria-label="Add note"
+                  disabled={newNote.value === ""}
+                  aria-disabled={newNote.value === "" ? "true" : "false"}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-plus-circle"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
+                  </svg>
+                </button>
+              </div>
+            </form>
           </div>
-        </form>
+        </div>
 
-        <main class="container-fluid">
-          <div class="row">
-            {notes.value.map((note) => (
-              <div class="col-sm-6 mb-3 mb-sm-0">
-                <div key={note.id} class="card mb-3">
-                  <div class="card-header">#{note.id}</div>
+        <div class="grid text-center row">
+          {notes.value.map((note) => (
+            <div class="col-sm-6 mb-3 mb-sm-0">
+              <div key={note.id} class="card mb-3 rounded">
+                <button
+                  type="button"
+                  class="position-absolute top-0 end-0 p-2 m-1 btn-close bg-secondary bg-opacity-10 rounded-pill"
+                  aria-label="Close"
+                ></button>
+                {editedNoteId.value === note.id ? (
                   <div class="card-body">
-                    {editedNoteId.value === note.id ? (
-                      <form onSubmit={editNote} class="row g-2">
-                        <input
-                          value={editedNote.value}
-                          onInput={(e) =>
-                            (editedNote.value = e.currentTarget.value)
-                          }
-                          class="form-control"
-                          id="edit-note"
-                          name="edit-note"
-                          aria-label="Edit note"
-                        />
-                        <div class="col mb-1">
+                    <form onSubmit={editNote}>
+                      <div class="hstack">
+                        <div class="p-2 flex-grow-1">
+                          <input
+                            value={editedNote.value}
+                            onInput={(e) =>
+                              (editedNote.value = e.currentTarget.value)
+                            }
+                            class="form-control"
+                            id="edit-note"
+                            name="edit-note"
+                            aria-label="Edit note"
+                          />
+                        </div>
+                        <div class="p-2 ms-auto">
                           <button
                             type="submit"
-                            class="btn btn-success"
+                            class="btn btn-sm btn-success"
                             aria-label="Save note"
                           >
                             <svg
@@ -309,10 +331,11 @@ export function App() {
                             </svg>
                           </button>
                         </div>
-                        <div class="col mb-1">
+                        <div class="vr"></div>
+                        <div class="p-2">
                           <button
                             type="reset"
-                            class="btn btn-danger"
+                            class="btn btn-sm btn-outline-danger"
                             onClick={() => {
                               editedNoteId.value = null;
                               editedNote.value = "";
@@ -332,65 +355,77 @@ export function App() {
                             </svg>
                           </button>
                         </div>
-                      </form>
-                    ) : (
-                      <>
-                        <h5 class="card-title">{note.content}</h5>
-                        <div class="row g-3">
-                          <div class="col mb-3">
-                            <button
-                              class="btn btn-primary"
-                              onClick={() => {
-                                editedNoteId.value = note.id;
-                                editedNote.value = note.content ?? "";
-                              }}
-                              aria-label="Edit note"
-                              disabled={editedNoteId.value !== null}
-                              aria-disabled={
-                                editedNoteId.value !== null ? "true" : "false"
-                              }
-                            >
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="16"
-                                height="16"
-                                fill="currentColor"
-                                class="bi bi-pencil"
-                                viewBox="0 0 16 16"
-                              >
-                                <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
-                              </svg>
-                            </button>
-                          </div>
-                          <div class="col mb-3">
-                            <button
-                              class="btn btn-danger"
-                              onClick={() => deleteNote(note.id)}
-                              aria-label="Delete note"
-                            >
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="16"
-                                height="16"
-                                fill="currentColor"
-                                class="bi bi-trash3"
-                                viewBox="0 0 16 16"
-                              >
-                                <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
-                              </svg>
-                            </button>
-                          </div>
-                        </div>
-                      </>
-                    )}
+                      </div>
+                    </form>
                   </div>
-                  <div class="card-footer text-body-secondary">{new Date(note.updated_at).toDateString()}</div>
+                ) : (
+                  <div class="card-body">
+                    <div class="hstack">
+                      <div class="p-2">
+                        <h5>{note.content}</h5>
+                      </div>
+                      <div class="p-2 ms-auto">
+                        <button
+                          class="btn btn-sm btn-primary"
+                          onClick={() => {
+                            editedNoteId.value = note.id;
+                            editedNote.value = note.content ?? "";
+                          }}
+                          aria-label="Edit note"
+                          disabled={editedNoteId.value !== null}
+                          aria-disabled={
+                            editedNoteId.value !== null ? "true" : "false"
+                          }
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            class="bi bi-pencil"
+                            viewBox="0 0 16 16"
+                          >
+                            <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
+                          </svg>
+                        </button>
+                      </div>
+                      <div class="vr"></div>
+                      <div class="p-2">
+                        <button
+                          class="btn btn-sm btn-danger"
+                          onClick={() => deleteNote(note.id)}
+                          aria-label="Delete note"
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            class="bi bi-trash3"
+                            viewBox="0 0 16 16"
+                          >
+                            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
+                          </svg>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                )}
+                <div class="card-footer text-body-secondary">
+                  Modified on: {new Date(note.updated_at).toDateString()}
                 </div>
               </div>
-            ))}
-          </div>
-        </main>
+            </div>
+          ))}
+        </div>
       </div>
+      <footer class="d-flex flex-wrap justify-content-between align-items-center p-3 my-4 border-top">
+        <div class="col-md-4 d-flex align-items-center">
+          <span class="mb-3 mb-md-0 text-body-secondary">
+            © 2025 Dragontailonfire
+          </span>
+        </div>
+      </footer>
     </div>
   );
 }
